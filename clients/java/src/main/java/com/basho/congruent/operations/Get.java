22e73541f42e75946a1a3585479763ad5384ec8e
Brian Roach <roach@basho.com>
2012-02-04 17:08:51 -0700

    revamped get and put to allow properties

--- clients/java/src/main/java/com/basho/congruent/operations/Get.java
+++ clients/java/src/main/java/com/basho/congruent/operations/Get.java
@@ -11,7 +11,9 @@ import com.basho.riak.client.RiakRetryFailedException;
 import com.basho.riak.client.bucket.Bucket;
 import com.basho.riak.client.cap.UnresolvedConflictException;
 import com.basho.riak.client.http.response.RiakIORuntimeException;
+import com.basho.riak.client.operations.FetchObject;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.commons.codec.binary.Base64;
@@ -36,6 +38,8 @@ public class Get extends RiakOperation {
         {
             Bucket bucket = client.fetchBucket(bucketName).execute();
 
+            
+            
             /* This gets a little weird because of how this interface is designed.
             * If there's siblings, it's going to throw an exception because we
             * aren't specifying a ConflictResolver (because we want to see the 
@@ -44,9 +48,52 @@ public class Get extends RiakOperation {
 
             List<IRiakObject> riakObjectList = new LinkedList<IRiakObject>();
 
+            FetchObject<IRiakObject> fetchObject = bucket.fetch(key);
+            
+            Iterator<String> fieldIterator = commandNode.getFieldNames();
+            
+            while (fieldIterator.hasNext())
+            {
+                String field = fieldIterator.next();
+                
+                if (field.equalsIgnoreCase("r"))
+                {
+                    int i = commandNode.get(field).getValueAsInt(-1);
+                    if (i == -1)
+                    {
+                        term.failOperation("symbolic quora not supported");
+                        return term.toString();
+                    }
+                    else
+                        fetchObject.r(i);
+                }
+                else if (field.equalsIgnoreCase("pr"))
+                {
+                    int i = commandNode.get(field).getValueAsInt(-1);
+                    if (i == -1)
+                    {
+                        term.failOperation("symbolic quora not supported");
+                        return term.toString();
+                    }
+                    else
+                        fetchObject.pr(i);
+                }
+                else if (field.equalsIgnoreCase("basic_quorum"))
+                {
+                    fetchObject.basicQuorum(commandNode.get(field).getBooleanValue());
+                }
+                else if (field.equalsIgnoreCase("notfound_ok"))
+                {
+                    fetchObject.notFoundOK(commandNode.get(field).getBooleanValue());
+                }
+                
+            }
+            
+            
+            
             try
             {
-                IRiakObject response = bucket.fetch(key).execute();
+                IRiakObject response = fetchObject.execute();
                 if (null != response)
                     riakObjectList.add(response);
             }
--- clients/java/src/main/java/com/basho/congruent/operations/Put.java
+++ clients/java/src/main/java/com/basho/congruent/operations/Put.java
@@ -6,8 +6,11 @@ package com.basho.congruent.operations;
 
 import com.basho.congruent.output.ErlangTerm;
 import com.basho.riak.client.IRiakClient;
+import com.basho.riak.client.IRiakObject;
 import com.basho.riak.client.RiakRetryFailedException;
 import com.basho.riak.client.bucket.Bucket;
+import com.basho.riak.client.operations.StoreObject;
+import java.util.Iterator;
 import org.apache.commons.codec.binary.Base64;
 
 /**
@@ -26,11 +29,67 @@ public class Put extends RiakOperation
         
         ErlangTerm term = new ErlangTerm(commandName, protocolName);
 
+        
+        
         try 
         {
             Bucket bucket = client.fetchBucket(bucketName).execute();
-            bucket.store(key,value).execute();
-            term.noResultData();
+            StoreObject<IRiakObject> storeObject = bucket.store(key,value);
+
+            Iterator<String> fieldIterator = commandNode.getFieldNames();
+            
+            
+            while (fieldIterator.hasNext())
+            {
+                String field = fieldIterator.next();
+                
+                if (field.equalsIgnoreCase("w"))
+                {
+                    int i = commandNode.get(field).getValueAsInt(-1);
+                    if (i == -1)
+                    {
+                        term.failOperation("symbolic quora not supported");
+                        return term.toString();
+                    }
+                    else
+                        storeObject.w(i);
+                }
+                else if (field.equalsIgnoreCase("dw"))
+                {
+                    int i = commandNode.get(field).getValueAsInt(-1);
+                    if (i == -1)
+                    {
+                        term.failOperation("symbolic quora not supported");
+                        return term.toString();
+                    }
+                    else
+                        storeObject.dw(i);
+                } 
+                else if (field.equalsIgnoreCase("pw"))
+                {
+                    int i = commandNode.get(field).getValueAsInt(-1);
+                    if (i == -1)
+                    {
+                        term.failOperation("symbolic quora not supported");
+                        return term.toString();
+                    }
+                    else
+                        storeObject.pw(i);
+                }
+                else if (field.equalsIgnoreCase("returnbody"))
+                {
+                    storeObject.returnBody(commandNode.get(field).getBooleanValue());
+                }
+                
+            }
+            
+            IRiakObject riakObject = storeObject.execute();
+            if (riakObject != null)
+            {
+                term.addByteArrayToResultData(riakObject.getValue());
+            }
+            else
+                term.noResultData();
         }
         catch (RiakRetryFailedException ex)
         {
